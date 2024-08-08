b118004195b5bf61b8a6ab2fb7b712f14f7f2d9a
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-08-08 14:40:14 -0700

    feat: implement responsive design for hero section

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,48 @@
+import { useEffect, useRef, useState } from 'react';
 import './Home.css';
+import { DotLottie, DotLottieReact } from '@lottiefiles/dotlottie-react';
+import hamburger from '../../assets/hamburger_menu.json';
 
 export default function(){
+    const [width, setWidth] = useState<number>(window.innerWidth);
+    const [dotlottie, setDotLottie] = useState<DotLottie|null|undefined>(null);
+    const [resetAnimation, setResetAnimation] = useState<boolean>(true);
+    const navRef = useRef<HTMLDivElement|null>(null);
+    const dotLottieRefCallback = (dotLottie:DotLottie) => {
+        setDotLottie(dotLottie)
+    };
+    const handleAnimation = ()=>{
+        const {current} = navRef;
+        if(dotlottie){
+            if(resetAnimation){
+                dotlottie.setSegment(0, 79);
+                setResetAnimation(false);
+                if(current){
+                    current.style.right = '-0vw';
+                }
+            }else{
+                dotlottie.setSegment(79, 130);
+                setResetAnimation(true);
+                if(current){
+                    current.style.right = '-100vw';
+                }
+            }
+            dotlottie.play();
+        }
+    }
+    const handleWidth = ()=>{
+        setWidth(window.innerWidth);
+    }
+    useEffect(()=>{
+        window.addEventListener('resize', handleWidth);
+        return ()=>window.removeEventListener('resize', handleWidth)
+    },[width])
     return <header className='hero'>
         <header className='header'>
-            <nav className='nav'>
+        {
+            width>800?undefined:<DotLottieReact autoplay={false} loop={false} onClick={handleAnimation} dotLottieRefCallback={dotLottieRefCallback} data={hamburger} className='nav__menu--btn'/>
+        }
+            <nav className='nav' ref={navRef}>
                 <span>bnj-rog</span>
                 <ul className='nav__list'>
                     <li className='nav__item'>Nosotros</li>
@@ -11,7 +50,7 @@ export default function(){
                     <li className='nav__item'>Proceso</li>
                     <li className='nav__item'>FAQ</li>
                 </ul>
-                <button>Servicios</button>
+                <button className='nav__cta'>Servicios</button>
             </nav>
         </header>
         <div>
@@ -23,7 +62,7 @@ export default function(){
                     <button className='hero__btn hero__btn--contact'>Contactanos</button>
                 </div>
             </section>
-            <section>
+            <section className='hero__image-cont'>
                 <img className='hero__image' src="https://res.cloudinary.com/dcezb5utw/image/upload/v1722969971/website/fxun8ltjezdrdfbtymzw.png" alt=""/>
             </section>
         </div>
