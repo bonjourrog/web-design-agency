5bb8793c635ac3d4a4933f5bd98e62c1fd4c4836
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-08-10 11:39:40 -0700

    feat: ceate business data mock

---
+++ src/Mocks/business.mock.ts
@@ -0,0 +1,28 @@
+import { Business } from "../Models/Business";
+
+
+export const BUSINESS_DATA_MOCK:Business[] = [
+    {
+        name: 'Joyat',
+        image: '',
+        font: 'Montserrat,Courier New,Courier,monospace'
+    },
+    {
+        name:'La Fat Burger',
+        image: 'https://res.cloudinary.com/dcezb5utw/image/upload/v1723230460/hq/e3abfvs7lt7o1ipyeyui.png',
+        width:'6em'
+    },
+    {
+        name:'Blue Bay Car Rental',
+        image: 'https://res.cloudinary.com/dzqoo2so5/image/upload/v1710458787/bluebay/images/logo/imozuqakjg9wfju3ym4v.png'
+    },
+    {
+        name: 'El comedero',
+        image: 'https://elcomedero.com.mx/wp-content/uploads/2024/05/WhatsApp-Image-2024-05-16-at-12.12-1El-comedor.png'
+    },
+    {
+        name: 'HQCONSTRUCCIONES',
+        image: 'https://res.cloudinary.com/dcezb5utw/image/upload/v1723230116/hq/uaf6ndnrg27hvyxijyr9.png'
+    }
+
+]
